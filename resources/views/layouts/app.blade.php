6651d64800e6fed13a40648bbc66e9429d23d2f5
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2019-01-24 20:49:32 +0300

    Added Yandex.Metrika counter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,5 +59,23 @@
 <script src="{{ asset('js/wNumb.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(52059306, "init", {
+        id:52059306,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/52059306" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
